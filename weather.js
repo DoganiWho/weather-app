42d1fe029a7981efe7109a7ead6d0260e77af422
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-04-13 10:52:02 +0000

    installed axios

---
+++ weather.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=ms&timeformat=unixtime&timezone=auto
+
+export function getWeather (lat, lon, timezone) {
+    axios.get('https://api.open-meteo.com/v1/forecast', {
+        params: {
+            latitude: lat,
+            longitude: lon, 
+            timezone,
+        }
+    })
+}
